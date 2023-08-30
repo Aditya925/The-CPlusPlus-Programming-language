e9792281c1eda749b2524f9dbe979936fc8d2615
Aditya925 <adityarahalkar925@gmail.com>
2023-08-30 10:34:10 +0530

    Memory pointers.cpp file upload

---
+++ CHAPTER 4/chap3_References.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/*
+    A reference in C++ is essentially an alias for an existing object. Once a reference is initialized 
+    with an object, it acts as a synonym for that object and can be used interchangeably. References 
+    are declared using the & symbol following the type of the object they refer to.
+*/
+
+int references(){
+    // Reference declaration and initialization
+    int original = 42;
+    int &reference = original;
+
+    std::cout << "Original: " << original << std::endl;
+    std::cout << "Reference: " << reference << std::endl;
+    std::cout << "Memory address of original: " << &original << std::endl;
+    std::cout << "Memory address of reference: " << &reference << std::endl;
+
+    // Modifying value through reference
+    reference = 100;
+    std::cout << "Modified Original: " << original << std::endl;
+}
--- CHAPTER 4/main.cpp
+++ CHAPTER 4/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "chap1_Memory_address.cpp"
 #include "chap2_Pointers.cpp"
+#include "chap3_References.cpp"
 
 int main(){
 
     // memory_address();
     // pointers();
+    // references();
 
     std::cout << "END OF PROGRAM" << std::endl;
     return 0;
