9bacbfcff3575c3121c725afcb9fb2f495257cb1
Aditya925 <adityarahalkar925@gmail.com>
2023-09-03 14:08:45 +0530

    changes in main.cpp

--- UNIT 4 - Memory/main.cpp
+++ UNIT 4 - Memory/main.cpp
@@ -9,22 +9,34 @@
 #include "ch_08_Dynamic_array.cpp"
 #include "ch_09_Recursion.cpp"
 #include "ch_10_Function_templates.cpp"
+#include "ch_11_RAII_principle.cpp"
+#include "ch_12_Smart_pointers.cpp"
+#include "ch_13_Memory_leaks.cpp"
 
 int main(){
 
-    // memory_address();
-    // pointers();
-    // references();
-    // const_parameters();
-    // pass_by_reference();
-    // null_pointer();
-    // dynamic_memory();
-    // dynamic_array();
-    /* 
-       recursion();
-       factorial();
-    */
-    // function_template();
+   // memory_address();
+   // pointers();
+   // references();
+   // const_parameters();
+   // pass_by_reference();
+   // null_pointer();
+   // dynamic_memory();
+   // dynamic_array();
+   /* 
+      recursion();
+      factorial();
+   */
+   // function_template();
+   // raii_principle();
+   /*
+      uniqueptr();
+      sharedptr();
+   */
+   /*
+      sol_1();
+      sol_2();
+   */
 
     std::cout << "END OF PROGRAM" << std::endl;
     return 0;
